2d2cf248b8928c7f752a2ada213939fa604959aa
michal materowski <michal.materowski@gmail.com>
2016-12-13 22:30:30 +0100

    Add project files.

---
+++ MagazynChemikaCNSLAPP/Class2.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazynChemikaCNSLAPP
+{
+    class MainMenu
+    {
+        private int input;
+		private static int choice;
+		static MainMenu AppController = new MainMenu();
+	
+		public static void Run()
+		{
+			switch (AppController.InputFromUser())
+			{
+				case 1:
+					{
+						ListManager.ShowStorage();
+					}
+					break;
+				case 2:
+					{
+						ListManager.AddItem(ItemChoice()); 
+					}
+					break;
+				case 3:
+					{
+						ListManager.DeleteItem();
+					}
+					break;
+					//case 4:
+					//	{
+					//		ChangeItem();
+					//	}
+					//	break;
+
+			}
+		}
+
+
+		public void Welcome()
+        {
+            Console.WriteLine("Welcome to your chemistry's magazine!");
+            Console.WriteLine("1.Check storage");
+            Console.WriteLine("2.Add item");
+            Console.WriteLine("3.Delete item");
+            Console.WriteLine("4.Change item");
+            Console.WriteLine("____________________________________");
+        }
+
+        public int InputFromUser()
+        {
+            if (GetValue())
+            {
+                CheckValue(input);
+            }
+            return input;
+        }
+
+        private bool GetValue()
+        {
+
+            {
+                try
+                {
+                    input = Int16.Parse(Console.ReadLine());
+                    return true;
+                }
+
+
+
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Value must be an integer!");
+                    return false;
+                }
+
+
+            }
+        }
+
+        private void CheckValue(int ValueToCheck)
+        {
+            ValueToCheck = this.input;
+            if (ValueToCheck < 1 || ValueToCheck > 4)
+            {
+                Console.WriteLine("Wrong choice");
+            }
+
+        }
+
+       
+
+        private static int ItemChoice()
+        {
+            Console.WriteLine("What do you want to store?");
+            Console.WriteLine("1. Beaker");
+            Console.WriteLine("2. Flask");
+            return (Int32.Parse(Console.ReadLine()));
+    
+        }
+
+ 
+
+    
+
+       
+
+        //public void Choice(int choice)
+        //{
+        //    switch (choice)
+        //    {
+        //        case 1:
+        //            { CheckStorage(); }
+        //            break;
+        //        case 2:
+        //            { AddItem(); }
+        //            break;
+        //        case 3:
+        //            { DeleteItem(); }
+        //            break;
+        //        case 4:
+        //            { ChangeItem(); }
+        //            break;
+
+        //    }
+
+
+    //}
+}
+}
---
+++ MagazynChemikaCNSLAPP/ListManager.cs
@@ -0,0 +1,115 @@
+﻿using MagazynChemikaCNSLAPP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class ListManager
+{
+	static List<Glassware> glass = new List<Glassware>();
+	private static int id;
+
+	public static void ShowStorage()
+	{
+		IsStorageEmpty();
+		PrintStorage();
+		SummarizeEquipment();
+	}
+
+	private static void IsStorageEmpty()
+	{
+		if (glass.Count == 0)
+		{ Console.WriteLine("Storage is empty"); }
+	}
+
+	private static void PrintStorage()
+	{
+		foreach (Glassware piece in glass)
+		{
+			Console.WriteLine("Name{0}, ID:{1}, Velocity: {2} ml, Price: {3}$", piece, piece.ThisItemID, piece.velocity, piece.price);
+		}
+	}
+
+	private static void SummarizeEquipment()
+	{
+		Console.WriteLine("__________________________________________________________");
+		Console.WriteLine("Total items:{0} \t Total value: {1:N2}$", Glassware.ItemCounter, Glassware.PriceOfAllGlassware);
+	}
+
+	public static void AddItem(int option)
+	{
+		if (option == 1)
+			glass.Add(Beaker.AddBeaker());
+		if (option == 2)
+			glass.Add(Flask.AddFlask());
+		else
+		{
+			Console.WriteLine("Wrong choice!");
+		}
+
+	}
+
+
+	public static void DeleteItem()
+	{
+		id = WhichItem();
+		GoThruList();
+
+	}
+
+	private static int WhichItem()
+	{
+		Console.WriteLine("Give ID of item which you intent to throw out");
+		int id = Int32.Parse(Console.ReadLine());
+		return id;
+	}
+
+	private static void GoThruList()
+	{
+		foreach (Glassware piece in glass)
+		{
+			if (piece.ThisItemID == id)
+			{
+				ConfirmDelete(piece);
+				break;
+			}
+
+
+		}
+	}
+
+	private static void ThrowOutThisItem(Glassware piece)
+	{
+		glass.Remove(piece);
+		Glassware.ItemCounter--;
+		Glassware.PriceOfAllGlassware = Glassware.PriceOfAllGlassware - piece.price;
+		Console.WriteLine("Item has been thrown out.");
+
+	}
+
+	private static void ConfirmDelete(Glassware piece)
+
+	{
+		Console.WriteLine("Do you want to throw out {0}, quality={1}? \n y/n?", piece.ToString(), piece.CurrentState);
+		string choice = Console.ReadLine();
+		if (choice == "y")
+		{
+			ThrowOutThisItem(piece);
+		}
+
+
+		else if (choice == "n")
+			Console.WriteLine("Item of id: {0} wasn't deleted", piece.ThisItemID);
+
+		else
+		{
+			Console.WriteLine("Wrong choice");
+		}
+
+		}
+	}
+
+	
+	
