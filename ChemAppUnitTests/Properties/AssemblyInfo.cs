2c95c87ee4ba2bf1b698f3459e3a001a38f692c6
mmaterowski <michal.materowski@gmail.com>
2018-10-05 17:31:32 +0200

    adding test project

---
+++ ChemAppUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChemAppUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChemAppUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a7b6277-6c7b-4e3c-8ee9-eb7f33064499")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- MagazynChemikaCNSLAPP/Class1.cs
+++ MagazynChemikaCNSLAPP/Class1.cs
@@ -6,90 +6,90 @@ using System.Threading.Tasks;
 
 namespace MagazynChemikaCNSLAPP
 {
-    abstract class Glassware
-    {
-        public float price;
-        public static float PriceOfAllGlassware;
-        public int velocity;
-        public static int ItemCounter= 0;
-        public int ThisItemID;
-        protected bool IsClean;
-        public string CurrentState;
-        protected int quality = 100;
+	abstract class Glassware
+	{
+		public float price;
+		public static float PriceOfAllGlassware;
+		public int velocity;
+		public static int ItemCounter= 0;
+		public int ThisItemID;
+		protected bool IsClean;
+		public string CurrentState;
+		protected int quality = 100;
 
-      
-        Random UsingGlassware = new Random();
+	  
+		Random UsingGlassware = new Random();
 
-        protected virtual void Wash(Glassware obj)
-        {
-            if (obj.IsClean)
-            {
-                Console.WriteLine("It's already clean!");
-                IsClean = false;
-            }
-            else
-            {
-                Console.WriteLine("The {0} is clean now",obj);
-                IsClean = true;
-            }
-        }
+		protected virtual void Wash(Glassware obj)
+		{
+			if (obj.IsClean)
+			{
+				Console.WriteLine("It's already clean!");
+				IsClean = false;
+			}
+			else
+			{
+				Console.WriteLine("The {0} is clean now",obj);
+				IsClean = true;
+			}
+		}
 
-        protected virtual void Use(Glassware obj)
-        {
-           if (obj.quality < 20)
-            {
-                Console.WriteLine("You cannot use this piece, it's broken and have to be disposed");
-            }
-           else
-            {
-                Console.WriteLine("You've used this piece of glassware, now it's dirty");
-                IsClean = false;
+		protected virtual void Use(Glassware obj)
+		{
+		   if (obj.quality < 20)
+			{
+				Console.WriteLine("You cannot use this piece, it's broken and have to be disposed");
+			}
+		   else
+			{
+				Console.WriteLine("You've used this piece of glassware, now it's dirty");
+				IsClean = false;
 
-                int temp = obj.UsingGlassware.Next(100);
-                    if (temp < obj.quality)
-                {
-                    obj.quality = temp;
-                    StateChanger(obj);
-                }
-                    
-                
-                
-                
-            }
-        }
+				int temp = obj.UsingGlassware.Next(100);
+					if (temp < obj.quality)
+				{
+					obj.quality = temp;
+					StateChanger(obj);
+				}
+					
+				
+				
+				
+			}
+		}
 
-        protected virtual void StateChanger(Glassware obj)
-        {
-            if (obj.quality == 100) { obj.CurrentState = "New"; }
-            else if (obj.quality >75) { obj.CurrentState = "Good"; }
-            else if (obj.quality >25)
-            {
-                obj.CurrentState = "Damaged";
-                Console.WriteLine("Your beaker is now damaged, but You can still use it");
-            }
-            else if (obj.quality <= 20)
-            {
-                obj.CurrentState = "ToTrash";
-                Console.WriteLine("You broke this piece !");
-            }
+		protected virtual void StateChanger(Glassware obj)
+		{
+			if (obj.quality == 100) { obj.CurrentState = "New"; }
+			else if (obj.quality >75) { obj.CurrentState = "Good"; }
+			else if (obj.quality >25)
+			{
+				obj.CurrentState = "Damaged";
+				Console.WriteLine("Your beaker is now damaged, but You can still use it");
+			}
+			else if (obj.quality <= 20)
+			{
+				obj.CurrentState = "ToTrash";
+				Console.WriteLine("You broke this piece !");
+			}
 
-        }
-         
-     
-     
-    }
+		}
+		 
+	 
+	 
+	}
 
-    class Beaker : Glassware
-    {
-        public Beaker(float price, int velocity)
-        {
-            this.price = price;
-            this.velocity = velocity;
-            ThisItemID = ItemCounter++;
-            quality = 100;
-            CurrentState = "New";
-            PriceOfAllGlassware += price;
-        }
+	class Beaker : Glassware
+	{
+		public Beaker(float price, int velocity)
+		{
+			this.price = price;
+			this.velocity = velocity;
+			ThisItemID = ItemCounter++;
+			quality = 100;
+			CurrentState = "New";
+			PriceOfAllGlassware += price;
+		}
 
 		public override string ToString()
 		{
@@ -98,8 +98,8 @@ namespace MagazynChemikaCNSLAPP
 		}
 
 		public static Beaker AddBeaker()
-        {
-            Console.WriteLine("Give velocity");
+		{
+			Console.WriteLine("Give velocity");
 			int vel = MainMenu.InputNumber();
 			if (vel > 0)
 			{
@@ -112,23 +112,23 @@ namespace MagazynChemikaCNSLAPP
 				Console.WriteLine("The velocity must be a number greater than 0, try again");
 				return AddBeaker();
 			}
-        }
+		}
 
 
-       
-    }
-    
-    class Flask : Glassware
-    {
-        public Flask(float price, int velocity)
-        {
-            this.price = price;
-            this.velocity = velocity;
-            ThisItemID = ItemCounter++;
-            quality = 100;
-            CurrentState = "New";
-            PriceOfAllGlassware += price;
-        }
+	   
+	}
+	
+	class Flask : Glassware
+	{
+		public Flask(float price, int velocity)
+		{
+			this.price = price;
+			this.velocity = velocity;
+			ThisItemID = ItemCounter++;
+			quality = 100;
+			CurrentState = "New";
+			PriceOfAllGlassware += price;
+		}
 
 		public override string ToString()
 		{
@@ -137,8 +137,8 @@ namespace MagazynChemikaCNSLAPP
 		}
 
 		public static Flask AddFlask()
-        {
-            Console.WriteLine("Give velocity");
+		{
+			Console.WriteLine("Give velocity");
 			int vel = MainMenu.InputNumber();
 			if (vel > 0)
 			{
@@ -154,7 +154,7 @@ namespace MagazynChemikaCNSLAPP
 		}
 
 
-    }
+	}
 
-    
+	
 }
